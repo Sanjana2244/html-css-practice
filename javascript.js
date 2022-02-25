5c9f3e969cef2aa56f341e315f4c914cdd563691
Sanjana2244 <72740039+Sanjana2244@users.noreply.github.com>
2022-02-25 18:47:56 +0500

    html and css practice

---
+++ javascript.js
@@ -0,0 +1,25 @@
+console.log("my javascript program");
+
+//function in javascript
+
+function greet(name){
+console.log(name + " is a good girl" );
+}
+
+let name ="sanjana";
+let name1 ="sanjana";
+let name2 ="sanjana";
+greet(name);
+greet(name1);
+greet(name2);
+
+// alert in javascript
+alert("this is a message");
+
+// prompt in javascript
+let A = prompt("what is your name","Guest");
+console.log(A);
+
+// confirm
+let deletepost = confirm("Do you really want to delete this post?");
+console.log(deletepost);
